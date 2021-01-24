52b9c03e3df1dab5ee4cc9f9dcb92cebfbb68283
Arthur Borikun <arturborikun2@gmail.com>
2021-01-24 17:43:08 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   apps : [{
     "name": "olx-crawler",
-    "cwd": "/home/arthurb/workspace/olx-crawler",
     "args": [
       "-jar",
       "bin/olx.jar"
@@ -12,8 +11,6 @@ module.exports = {
     "log_date_format": "YYYY-MM-DD HH:mm Z",
     "exec_interpreter": "none",
     "exec_mode": "fork",
-    "error_file": "err.log",
-    "out_file": "out.log",
-    "log_file": "combined.log"
+    "log_file": "logs.log"
   }],
 };
